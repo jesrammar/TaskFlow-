5f5ed88cc23587673d5475b6ff1b19bc2b5cf413
jefer12betis <92800756+jefer12betis@users.noreply.github.com>
2025-02-05 13:54:50 +0100

    Tailwindcss configured sucessfully!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App(){
   return(
-    <div>App</div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element= {<h1>Home Page</h1>}/>
+          <Route path="/login" element= {<h1>Login</h1>}/>
+          <Route path="/register" element= {<h1>Register</h1>}/>
+          <Route path="/tasks" element= {<h1>Tasks</h1>}/>
+          <Route path="/add-task" element= {<h1>Add Task</h1>}/>
+          <Route path="/tasks/:id" element= {<h1>Update Task</h1>}/>
+          <Route path="/profile" element= {<h1>Prifile</h1>}/>
+        </Routes>
+      <h1 className="text-4xl font-bold">Hola mundo</h1>
+    </BrowserRouter>
   )
 }
 
